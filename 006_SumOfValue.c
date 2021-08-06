ed629a055417d40f7c7eff509d43de10e3949134
DibrajSarkar <hi.dibraj@gmail.com>
2021-08-07 00:10:14 +0600

    Sum of Given three values

---
+++ 006_SumOfValue.c
@@ -0,0 +1,12 @@
+//Write a program that take three integer variable a,b and c. Assign values to the variables a=10,b=20,c=30. Now print the sum of these three variables.
+
+#include<stdio.h>
+int main()
+{
+    int a=10,b=20,c=30,sum;
+    sum=(a+b+c);
+    printf("Sum of a,b and c values: %d\n",sum);
+
+
+    return 0;
+}
