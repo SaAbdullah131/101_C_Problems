a76026b9cfb71922aba3e8fcf5a15176b4056ff9
DibrajSarkar <hi.dibraj@gmail.com>
2021-08-07 00:44:14 +0600

    Numeric to Character

---
+++ 009_NumricToChar.c
@@ -0,0 +1,10 @@
+// Write a program that print "Bangladesh" without quotationsign by using ascii values
+
+#include<stdio.h>
+int main()
+{
+    printf("%c%c%c%c%c%c%c%c%c%c\n",66,97,110,103,108,97,100,101,115,104);
+
+
+    return 0;
+}
