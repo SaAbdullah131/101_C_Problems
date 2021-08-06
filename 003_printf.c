88ac025ae66f5e10aea5697baf3177a49bd4ca56
DibrajSarkar <hi.dibraj@gmail.com>
2021-08-06 23:58:11 +0600

    Printf problem solve simple

---
+++ 003_printf.c
@@ -0,0 +1,10 @@
+// Write a program to print name in one line and another line university name
+
+#include<stdio.h>
+int main()
+{
+    printf("Name: Mr. SA Abdullah\n"); // \n menas it's print a new line 
+    printf("University: European University of Bangladesh\n");
+
+    return 0;
+}
