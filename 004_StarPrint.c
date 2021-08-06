d53a9958587341b8a67e4618efeea4c9426eb0d8
DibrajSarkar <hi.dibraj@gmail.com>
2021-08-07 00:00:11 +0600

    Star printing problem

---
+++ 004_StarPrint.c
@@ -0,0 +1,18 @@
+// Write a program to print * and show following output.
+
+#include<stdio.h>
+int main()
+{
+
+printf("    *    \n");
+printf("   ***   \n");
+printf("  *****   \n");
+printf(" *******  \n");
+printf("********* \n");
+printf("  ********  \n");
+printf("   *****   \n");
+printf("   ***   \n");
+printf("    *    \n");
+
+    return 0;
+}
