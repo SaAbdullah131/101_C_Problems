27bd5e73ed1f54768ea44f6ce03808ea410cb0c0
DibrajSarkar <hi.dibraj@gmail.com>
2021-08-07 22:58:53 +0600

    Area of a circle

---
+++ 011_areaOfCircle.c
@@ -0,0 +1,14 @@
+// Write a program that give output of a circle
+
+#include<stdio.h>
+int main()
+{
+    double pi=3.1416;
+    int redius=5;
+    double sum=pi*redius*redius;
+
+    printf("Area of a Circle is: %d\n",sum);
+
+
+    return 0;
+}
