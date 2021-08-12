910651fc1dde108ba6e0a704560612adb68857ea
DibrajSarkar <hi.dibraj@gmail.com>
2021-08-12 23:52:23 +0600

    Check Vowel or Consonant

---
+++ 022_Vowel.c
@@ -0,0 +1,16 @@
+// Take a small letter alphabet as input and print whether it is vowel or consonant.
+#include<stdio.h>
+ int main(){
+      char q;
+      printf("Enter Your Desire Character: \n");
+      scanf("%c",&c);
+    if(q=='a' || q=='e'|| q=='i' || q='=o' || q==u){
+        printf("%c is vowel\n",q);
+    }
+    else
+    {
+        printf("%c is Consonant\n",q);
+    }
+    
+     return 0;
+ }
