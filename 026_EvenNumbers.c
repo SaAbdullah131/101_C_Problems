95c9f1f27baeb447a8039dddbee22ff4cc88b09a
DibrajSarkar <hi.dibraj@gmail.com>
2021-08-13 23:24:04 +0600

    Print even number between 1 to 100

---
+++ 026_EvenNumbers.c
@@ -0,0 +1,15 @@
+// print the EVEN Number between 1 to 100 using loop.
+#include<stdio.h>
+int main()
+{   int i;
+for ( i =1; i <=100; i++)
+{
+    if (i%2==0)
+    {
+        printf("%d",i);
+    }
+    
+}
+
+    return 0;
+}
